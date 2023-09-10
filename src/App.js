4b5d2cc6a9d7227d12394482e87183b295e5b9a2
nskulkarni11 <nskulkarni11@gmail.com>
2023-09-10 23:10:57 +0530

    intital commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full min-h-[100vh]">
+      <p className='text-blue'>Hello</p>
+    </div>
+  );
+}
+
+export default App;
